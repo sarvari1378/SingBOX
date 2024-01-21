a09637fb611969ce780ff8028cbb0100d6ad5501
sarvari1378 <137143835+sarvari1378@users.noreply.github.com>
2024-01-21 11:56:03 +0330

    Update REALITY-Creator.py

--- Scripts/REALITY-Creator.py
+++ Scripts/REALITY-Creator.py
@@ -165,7 +165,7 @@ User_url = 'https://raw.githubusercontent.com/sarvari1378/SingBOX/main/Users.txt
 
 users = get_users(User_url)
 urls = [
-    "https://raw.githubusercontent.com/yebekhe/TelegramV2rayCollector/main/sub/normal/vmess"
+    "https://api.yebekhe.link/shervin/"
 ]
 
 responses = get_config(urls)

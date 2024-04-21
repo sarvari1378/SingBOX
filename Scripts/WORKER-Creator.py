5cc56181eb365e4e1f3b35df64225c710d0d3f8e
sarvari1378 <137143835+sarvari1378@users.noreply.github.com>
2024-04-21 23:09:21 +0330

    Update WORKER-Creator.py

--- Scripts/WORKER-Creator.py
+++ Scripts/WORKER-Creator.py
@@ -248,6 +248,6 @@ def Create_SUBs(users, responses, PROTOCOL):
 
 User_url = 'https://raw.githubusercontent.com/sarvari1378/SingBOX/main/Users.txt'
 users = get_users(User_url)
-urls = ['https://af5734d8-de4d-4ce3-9572-be8d6fc682ae.qconnect.top/6a7952bd-1d7d-4f87-7bcd-223358afd3f9U083']
+urls = ['https://af5734d8-de4d-4ce3-9572-be8d6fc682ae.qconnect.top/2d1190a3-3395-4135-b6af-d68fc2de302eU102']
 responses = get_config(urls)
 Create_SUBs(users, responses, 'WORKER')

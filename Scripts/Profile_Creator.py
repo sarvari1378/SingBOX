6f274c7ce436719abea2179d379884950ffbbe6d
sarvari1378 <137143835+sarvari1378@users.noreply.github.com>
2024-08-10 10:11:01 +0330

    Update Profile_Creator.py

--- Scripts/Profile_Creator.py
+++ Scripts/Profile_Creator.py
@@ -1,7 +1,6 @@
 # libraries
-from datetime import datetime, timedelta
-import requests
 from datetime import datetime
+import requests
 import pytz
 import regex
 from collections import namedtuple
@@ -9,7 +8,8 @@ import os
 import base64
 import json
 import jdatetime
-import binascii 
+import binascii
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 def add_base64_padding(base64_string):
     padding_needed = 4 - (len(base64_string) % 4)
@@ -17,16 +17,23 @@ def add_base64_padding(base64_string):
         base64_string += "=" * padding_needed
     return base64_string
 
+def fetch_url(url):
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            return url, response.text
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+    return url, None
+
 def get_config(urls):
     responses = {}
-    for url in urls:
-        if url:
-            try:
-                response = requests.get(url)
-                if response.status_code == 200:
-                    responses[url] = response.text
-            except requests.exceptions.RequestException as e:
-                print(f"An error occurred: {e}")
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        future_to_url = {executor.submit(fetch_url, url): url for url in urls}
+        for future in as_completed(future_to_url):
+            url, content = future.result()
+            if content:
+                responses[url] = content
     return responses
 
 def merge_content(responses):
@@ -76,20 +83,18 @@ def Vmess_rename(vmess_config, new_name):
 def rename_configs(content, name):
     lines = content.split('\n')
     new_lines = []
+    now = datetime.now(pytz.timezone('Asia/Tehran'))
+    hour = now.strftime('%H:%M')
+    date = jdatetime.date.today().strftime('%Y-%m-%d')
+    
     for i, line in enumerate(lines):
         if line.startswith('vmess://'):
             flag = extract_flag(line)
-            now = datetime.now(pytz.timezone('Asia/Tehran'))
-            hour = now.strftime('%H:%M')
-            date = jdatetime.date.today().strftime('%Y-%m-%d')
             new_name = f'|{flag}|{hour}|{date}|{name}|{i+1}|'
             line = Vmess_rename(line, new_name)
         elif '#' in line:
             flag = extract_flag(line)
             line = line.split('#')[0]
-            now = datetime.now(pytz.timezone('Asia/Tehran'))
-            hour = now.strftime('%H:%M')
-            date = jdatetime.date.today().strftime('%Y-%m-%d')
             line += f'#|{flag}|{hour}|{date}|{name}|{i+1}|'
         new_lines.append(line)
     return '\n'.join(new_lines)
@@ -110,12 +115,16 @@ def get_users(url):
             users.append(User(items[0].strip(), items[1].strip()))
     return users
 
+def write_to_file(filename, content):
+    with open(filename, 'w') as f:
+        f.write(content)
+
 def Create_SUBs(users, responses, protocol_name, links=None):
     if not os.path.exists('SUB'):
         os.makedirs('SUB')
     
+    tasks = []
     if links is None:
-        # Process when 'Split' is False
         for user in users:
             if float(user.date) <= 0:
                 content = 'vless://64694D4A-2C05-4FFE-AEF1-68C0169CCCB7@146.248.115.39:443?encryption=none&fp=firefox&mode=gun&pbk=TXpA-KUEqsg6YlZUXf0gZIe14rFjKZZNAqWzjruNoh8&security=reality&serviceName=&sid=790D3C76&sni=www.speedtest.net&spx=%2F&type=grpc#اشتراک شما به پایان رسیده است.'
@@ -125,10 +134,12 @@ def Create_SUBs(users, responses, protocol_name, links=None):
                 line = f'vless://64694D4A-2C05-4FFE-AEF1-68C0169CCCB7@146.248.115.39:443?encryption=none&fp=firefox&mode=gun&pbk=TXpA-KUEqsg6YlZUXf0gZIe14rFjKZZNAqWzjruNoh8&security=reality&serviceName=&sid=790D3C76&sni=www.speedtest.net&spx=%2F&type=grpc#|👤نام: {user.username}|⌛️روز های باقی مانده: {user.date}|'
                 content = line + '\n' + content
             filename = f'SUB/{protocol_name}-{user.username}'
-            with open(filename, 'w') as f:
-                f.write(content)
+            tasks.append((filename, content))
+        
+        with ThreadPoolExecutor(max_workers=10) as executor:
+            for filename, content in tasks:
+                executor.submit(write_to_file, filename, content)
     else:
-        # Process when 'Split' is True
         num_links = len(links)
         num_users = len(users)
         
@@ -136,7 +147,6 @@ def Create_SUBs(users, responses, protocol_name, links=None):
             print(f"No links available for protocol: {protocol_name}")
             return
 
-        # Calculate the number of users per link
         users_per_link = num_users // num_links
         remainder = num_users % num_links
 
@@ -144,20 +154,18 @@ def Create_SUBs(users, responses, protocol_name, links=None):
         for i, link in enumerate(links):
             if i >= num_links:
                 break
-            # Determine the range of users for this link
+            
             start_index = user_index
             end_index = user_index + users_per_link + (1 if i < remainder else 0)
             link_users = users[start_index:end_index]
             user_index = end_index
             
-            # Get content for the current link
             if link in responses:
                 content = responses[link]
             else:
                 print(f"No content found for link: {link}")
                 continue
 
-            # Process content and create SUB files for users
             merged_content = merge_content({link: content})
             for user in link_users:
                 if float(user.date) <= 0:
@@ -168,8 +176,11 @@ def Create_SUBs(users, responses, protocol_name, links=None):
                     content = line + '\n' + content
 
                 filename = f'SUB/{protocol_name}-{user.username}'
-                with open(filename, 'w') as f:
-                    f.write(content)
+                tasks.append((filename, content))
+
+        with ThreadPoolExecutor(max_workers=10) as executor:
+            for filename, content in tasks:
+                executor.submit(write_to_file, filename, content)
 
 # Read JSON configuration file
 json_file_path = 'Jsons/config.json'  # Adjust the path to your JSON file
